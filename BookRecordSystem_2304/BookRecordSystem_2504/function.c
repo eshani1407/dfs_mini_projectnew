adf6ddda8fc3c8cfbb52739a2689f325dfb04489
Eshani Shet Kurtarkar <34548498+eshani1407@users.noreply.github.com>
2018-04-25 10:23:10 +0000

    Add files via upload

---
+++ BookRecordSystem_2304/BookRecordSystem_2504/function.c
@@ -0,0 +1,328 @@
+/*
+*Author         : Eshani Shet Kurtarkar(1726)
+                  Sameer Gaounkar
+*Program        : Implementation of Storing records of a book using threaded binary tree (ADT Specifications).
+*Modified Date  : 25th April 2018 
+*/
+ 
+
+
+
+#define SIZE 100
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include"header.h"
+
+
+
+/**********************************************************************************************************************************************/
+
+//to get the valid response from the user
+char getValidResponse()
+{
+	char t;
+	do{
+		getchar();
+		scanf("%c",&t);
+		t=toupper(t);
+		if(t=='Y' || t=='N')
+		 {
+			 break;
+		 }
+		else
+		 {
+		  	printf("invalid respose enter Y or N\n");
+		 }
+	}
+	while(1);
+	return t;
+}
+
+
+
+/**********************************************************************************************************************************************/
+
+// to display the program qts.
+void printIntro(char *intro)
+{
+	printf("\n-------------------------------------------------------\n");	
+	printf("\n%s\n",intro);
+	printf("\n--------------------------------------------------------\n");
+}
+
+
+/*****************************************************************************************************************************/
+
+/*
+**
+**ADT Specification: createTree() 
+**
+**		Function	:Initializes tree to an empty state.
+**		Preconditions   : None
+**		Input           : None
+**		Output          : tree(tbt_type)
+**		Postconditions	: tree exists and is empty.
+**
+*/
+
+tbt_type createTree()
+{
+        //tree=NULL;
+	return tree;
+}
+
+/*****************************************************************************************************************************/
+
+/*
+**
+**ADT Specification: insertData() 
+**
+**		Function        : Adds new book record to the existing records
+**		Preconditions   : tree is existing
+**		Input           : tree(tbt_type)
+			          newdata(data_type)
+**		Output          : tree(tbt_type)
+**		Postconditions	: tree=(existing tree)+(newdata).
+**
+*/
+tbt_type insertData(tbt_type tree,data_type newdata)
+{
+
+        tbtNodeType *newnode;
+	int item;
+	
+	item=newdata.book_id;
+	
+        find(item,&parent,&location);
+
+        if(location!=NULL)
+        {
+                printf("\nBook is already added in record\n");
+                return tree;
+        }
+        
+        newnode=(tbtNodeType *)malloc(sizeof(tbtNodeType));
+        if(newnode==NULL)
+        {
+                printf("\nERROR\n");
+                exit(-1);
+        }
+                
+        newnode->info=newdata;
+        newnode->left=thread;
+        newnode->right=thread;
+
+        if(parent==tree) /*tree is empty*/
+        {
+                tree->left=link;
+                tree->left_ptr=newnode;
+                newnode->left_ptr=tree;
+                newnode->right_ptr=tree;
+        }
+        
+        else if( item < parent->info.book_id )
+        {
+                newnode->left_ptr=parent->left_ptr;
+                newnode->right_ptr=parent;
+                parent->left=link;
+                parent->left_ptr=newnode;
+        }
+        else
+        {
+                newnode->left_ptr=parent;
+                newnode->right_ptr=parent->right_ptr;
+                parent->right=link;
+                parent->right_ptr=newnode;
+        }
+        
+        printf("record\n");
+        return tree;
+}/*End of insert()*/
+
+
+ /*****************************************************************************************************************************/
+/*
+**
+**ADT Specification: find() 
+**
+**		Function	:Search the book based on book_id.
+**		Preconditions   : tree is existing
+**		Input           : item(int)
+                                  **par(tbtNodeType)
+                                  **loc(tbtNodeType)
+**		Output          : true(1) or false(0)
+**		Postconditions	: none		//no modifications done to the tree
+**
+*/
+
+int find(int item,tbtNodeType **par,tbtNodeType **loc)
+{
+    tbtNodeType *ptr,*ptrsave;
+    
+    if(tree->left_ptr==tree)  /* If tree is empty*/
+    {
+        *loc=NULL;
+        *par=tree;
+        return 1;
+    }
+    
+    if(item==tree->left_ptr->info.book_id) /* item is at tree->left_ptr */
+    {
+        *loc=tree->left_ptr;
+        *par=tree;
+        return 1;
+    }
+    
+    ptr=tree->left_ptr;
+    
+    while(ptr!=tree)
+    {
+        ptrsave=ptr;
+        if( item < ptr->info.book_id)           /*left sub travel*/
+        {       
+            if(ptr->left==link)
+            {
+                ptr=ptr->left_ptr;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        else if(item > ptr->info.book_id)
+        {
+            if(ptr->right==link)
+            {
+                ptr=ptr->right_ptr;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        
+        if(item==ptr->info.book_id)
+        {
+            *loc=ptr;
+            *par=ptrsave;
+            return 0;
+        }
+                
+    }/*End of while*/
+    
+    *loc=NULL;   /*item not found*/
+    *par=ptrsave;
+}/*End of find()*/
+
+/*****************************************************************************************************************************/
+
+/*DUMMY root node*/
+
+tbt_type insert_tree()
+{
+        struct tbtNodeType *tmp;
+        tree=(tbtNodeType *)malloc(sizeof(tbtNodeType));
+        tree->info.book_id= 9999;                //  tree->info= infinity;
+        tree->left=thread;
+        tree->left_ptr=tree;
+        tree->right=link;
+        tree->right_ptr=tree;
+}/*End of insert_tree()*/
+
+
+/*****************************************************************************************************************************/
+
+
+/* Finding succeeder */
+
+tbtNodeType *in_succ(tbtNodeType *ptr)
+{
+    tbtNodeType *succ;
+    if(ptr->right==thread)
+        succ=ptr->right_ptr;
+    else
+    {
+        ptr=ptr->right_ptr;
+        while(ptr->left==link)
+            ptr=ptr->left_ptr;
+        succ=ptr;
+    }
+    return succ;
+}/*End of in_succ()*/
+
+
+/*****************************************************************************************************************************/
+
+/* Finding predecessor */
+
+tbtNodeType *in_pred(tbtNodeType *ptr)
+{
+    tbtNodeType *pred;
+    if(ptr->left==thread)
+        pred=ptr->left_ptr;
+    else
+    {
+        ptr=ptr->left_ptr;
+        while(ptr->right==link)
+            ptr=ptr->right_ptr;
+        pred=ptr;
+    }
+    return pred;
+}/*End of in_pred()*/
+
+
+
+/*****************************************************************************************************************************/
+
+/*
+**
+**ADT Specification: printInorder() 
+**
+**		Function	: prints all the book records in in-order fashion
+				  (i.e. left->root->right)
+**		Preconditions   : tree is existing
+**		Input           : tree(tbt_type)
+**		Output          : lists all the records
+**		Postconditions	: none 		//no modifications done to the tree
+**
+*/
+
+
+/*CHANGES DONE HERE*/
+
+/* Displaying all nodes */
+
+tbt_type inorder()
+{
+    tbtNodeType *ptr;
+    if(tree->left_ptr==tree)
+    {
+        printf("Tree is empty");
+        return tree;
+    }
+
+    ptr=tree->left_ptr;
+
+
+    /*Find the leftmost node and traverse it */
+
+    while(ptr->left==link)
+    {
+        ptr=ptr->left_ptr;
+    }
+    printf("%d |%c | %c |%c\n",ptr->info.book_id,ptr->info.book_name, ptr->info.book_type, ptr->info.book_author);
+
+    while( 1 )
+    {
+        ptr=in_succ(ptr);
+        if(ptr==tree)     /*If last node reached */
+            break;
+        printf("%d",ptr->info);
+    } /*End of while*/
+    
+    return tree;
+}/*End of inorder()*/
+
---
+++ BookRecordSystem_2304/BookRecordSystem_2504/main.c
@@ -0,0 +1,65 @@
+/*
+*Author         : Eshani Shet Kurtarkar(1726)
+                  Sameer Gaounkar
+*Program        : Implementation of Storing records of a book using threaded binary tree (ADT Specifications).
+*Modified Date  : 24th April 2018 
+*/
+ 
+#include<stdlib.h>
+#include<stdio.h>
+#include"header.h"
+
+int main()
+{
+	int choice,opt;
+	boolean flag;
+	data_type info,temp;
+	char response;
+	tbt_type  tree=NULL;
+	
+	printIntro("Implementation of storing records of a book using threaded binary tree");
+	tree=createTree();
+	
+	printf("\n Tree is created\n");	
+        insert_tree();
+
+	do
+	{
+		printf("\nWhat would you like to do?\n");
+		printf("\t1: Insert New book record\n");
+		printf("\t2: Search a Book\n");
+		printf("\t3: Print all books\n");
+		printf("\t4: Exit\n");	
+
+	
+		printf("\n\t\tEnter Your Choice:");
+		scanf("%d",&choice);
+	
+		switch(choice)
+		{
+		      case 1:printf("\nDetails of a book\n");
+		               printf("  Book-id :");
+		               scanf("%d",&info.book_id);
+		               printf("\n  Name of the book: ");
+			       getchar();
+			       scanf("%c",&info.book_name);
+			       printf("\n  Authore of the book: ");
+			       getchar();
+			       scanf("%c",&info.book_author);   
+			       printf("\n  Type of the book: ");
+			       getchar();
+			       scanf("%c",&info.book_type);
+			       tree=insertData(tree,info);	       
+			       break;                   
+                        case 3: inorder();
+                        case 4: exit(-1);	
+			default:printf("\nWrong choice, Please enter the correct option.");
+		}
+		
+		printf("\nWant to continue? (Y/N)");
+		response=getValidResponse();
+        }
+        while(response=='Y');
+
+    return 0;
+}
